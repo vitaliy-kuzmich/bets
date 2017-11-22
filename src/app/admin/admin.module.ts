025c2d84ac0110738e823d8a26390717f20dafa6
vitaliy.kuzmich@gmail.com <yabadabad0)>
2017-11-22 15:42:23 +0200

    fix prod build

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,11 +3,26 @@ import {CommonModule} from '@angular/common';
 import {AddTeamComponent} from "./add-team/add-team.component";
 import {AddGameComponent} from "./add-game/add-game.component";
 import {DataTypesModule} from "../data-types/data-types.module";
+import {
+  MatButtonModule, MatCardModule, MatDatepickerModule, MatGridListModule, MatInputModule,
+  MatSelectModule
+} from "@angular/material";
+import {FormsModule} from "@angular/forms";
+import {BrowserModule} from "@angular/platform-browser";
 
 @NgModule({
   imports: [
     CommonModule,
     DataTypesModule,
+    BrowserModule,
+    FormsModule,
+
+    MatCardModule,
+    DataTypesModule,
+    MatButtonModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
   ],
   declarations: [AddTeamComponent, AddGameComponent],
 })
